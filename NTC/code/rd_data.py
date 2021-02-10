88b93bbe7db87285e5b002ce29d73506e29718c0
vivekcvarghese <vivekcvarghese123@gmail.com>
2021-02-10 13:57:58 +0530

    updated task name

--- NTC/code/rd_data.py
+++ NTC/code/rd_data.py
@@ -2,16 +2,18 @@ import MySQLdb
 
 def RdData(filters = 0):
     
-    if filters != 0:
+    if filters != 0 and len(filters) != 0:
 
             sql_list = str(tuple([key for key in filters])).replace(',)', ')')
             foo = "AND SLAExpiration IN {}".format(sql_list)
+            too = "WHERE SLAExpiration IN {}".format(sql_list)
     else:
             foo = ""
+            too = ""
     database = MySQLdb.connect (host="localhost", user = "root", passwd = "root", db = "ntc")
     cursor = database.cursor()
 
-    query1 = "SELECT DISTINCT(Task_Name) FROM data ORDER BY Task_Name"
+    query1 = "SELECT DISTINCT(Task_Name) FROM data {} ORDER BY Task_Name".format(too)
     cursor.execute(query1)
     result = cursor.fetchall()  
 
