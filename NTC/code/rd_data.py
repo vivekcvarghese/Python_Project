4bdb47c117cca2925975857247327f5d47d28cf9
vivekcvarghese <vivekcvarghese123@gmail.com>
2021-02-11 14:27:33 +0530

    date filter modified

--- NTC/code/rd_data.py
+++ NTC/code/rd_data.py
@@ -1,19 +1,17 @@
 import MySQLdb
 
-def RdData(filters = 0):
+def RdData(filters = []):
     
-    if filters != 0 and len(filters) != 0:
+    if len(filters) != 0:
 
             sql_list = str(tuple([key for key in filters])).replace(',)', ')')
             foo = "AND SLAExpiration IN {}".format(sql_list)
-            too = "WHERE SLAExpiration IN {}".format(sql_list)
     else:
             foo = ""
-            too = ""
     database = MySQLdb.connect (host="localhost", user = "root", passwd = "root", db = "ntc")
     cursor = database.cursor()
 
-    query1 = "SELECT DISTINCT(Task_Name) FROM data {} ORDER BY Task_Name".format(too)
+    query1 = "SELECT DISTINCT(Task_Name) FROM data WHERE Task_Status = 'Available' {} ORDER BY Task_Name".format(foo)
     cursor.execute(query1)
     result = cursor.fetchall()  
 
@@ -22,9 +20,17 @@ def RdData(filters = 0):
     dates = []
     for row in result:
             tasks.append(row[0])
-    
+
+    if len(filters) == 0:
+        query2 = "SELECT DISTINCT(SLAExpiration) FROM data WHERE Task_Status = 'Available'"
+        cursor.execute(query2)
+        result = cursor.fetchall()
+        for row in result:
+                dt1 = row[0].strftime("%Y/%m/%d")
+                dates.append(dt1)
+
     for i in tasks:
-        query = "SELECT State, COUNT(State), SLAExpiration FROM data  WHERE Task_Name = '{}' AND Task_Status = 'Available' {} GROUP BY State ORDER BY State".format(i,foo)
+        query = "SELECT State, COUNT(State) FROM data  WHERE Task_Name = '{}' AND Task_Status = 'Available' {} GROUP BY State ORDER BY State".format(i,foo)
         cursor.execute(query)
         result = cursor.fetchall()
 
@@ -34,11 +40,7 @@ def RdData(filters = 0):
         for row in result:
                 total_count += row[1]
                 state[row[0]] = row[1]
-                dt1 = row[2].strftime("%Y/%m/%d")
-                if dt1 in dates:
-                        continue
-                else:   
-                        dates.append(dt1)
+
         state['Grand_total'] = total_count
         output.append(state)
 
