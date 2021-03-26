73fa416858833bddef8a155790b244ff299dd207
vivekcvarghese <vivekcvarghese123@gmail.com>
2021-03-26 12:02:58 +0530

    check for roles...

--- NTC/code/user.py
+++ NTC/code/user.py
@@ -12,31 +12,30 @@ from ldap3 import *
 class User(Resource):
 
     def post(self):
-        return {"login":"success", 
-                    "name":"Vivek C Varghese",
-                    "account_name":"CH20005",
-                    "description":"d",
-                    "cn":"User"}
+
         jdata = request.get_json()
         username = jdata['username']
         pswd = jdata['password']
-        AccountName = username.split("\\")[1]
-
+        
         try:
-            
+            AccountName = username.split("\\")[1]
             server = Server('ntcbpohyd.com')
             conn = Connection(server, user=username, password=pswd, auto_bind=True)
-            conn.search('dc=ntcbpohyd,dc=com','(&(objectclass=person)(sAMAccountName ='+AccountName+'))', attributes=['displayName', 'description'])
-            Name = str(conn.entries[0].displayName)
-            description = str(conn.entries[0].description)
-            
-            return {"login":"success", 
-                    "name":Name,
-                    "account_name":AccountName, 
-                    "description":description}
+            conn.search('dc=ntcbpohyd,dc=com','(&(objectclass=person)(sAMAccountName ='+AccountName+'))', attributes=['displayName', 'description', 'cn', 'distinguishedName'])
         
         except:
 
             return {"login":"invalid credentials"}
         
+        Name = str(conn.entries[0].displayName)
+        description = str(conn.entries[0].description)
+        if (description == '[]'):
+            description = ""
+            
+        data = conn.entries[0].entry_attributes_as_dict
+        data = data['distinguishedName']
 
+        return {"login":"success", 
+                    "name":Name,
+                    "account_name":AccountName, 
+                    "description": description}
